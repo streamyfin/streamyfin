ab603e699706eba91e450d78c644346fd0ffbba7
Fredrik Burmester <fredrik.burmester@gmail.com>
2025-01-11 10:09:53 +0100

    feat: add centralised plugin info

--- app/(auth)/(tabs)/(home)/intro/page.tsx
+++ app/(auth)/(tabs)/(home)/intro/page.tsx
@@ -5,7 +5,7 @@ import { Feather, Ionicons } from "@expo/vector-icons";
 import { Image } from "expo-image";
 import { useFocusEffect, useRouter } from "expo-router";
 import { useCallback } from "react";
-import { TouchableOpacity, View } from "react-native";
+import { Linking, TouchableOpacity, View } from "react-native";
 
 export default function page() {
   const router = useRouter();
@@ -17,7 +17,7 @@ export default function page() {
   );
 
   return (
-    <View className="bg-neutral-900 h-full py-32 px-4 space-y-4">
+    <View className="bg-neutral-900 h-full py-16 px-4 space-y-8">
       <View>
         <Text className="text-3xl font-bold text-center mb-2">
           Welcome to Streamyfin
@@ -85,25 +85,55 @@ export default function page() {
             </Text>
           </View>
         </View>
+        <View className="flex flex-row items-center mt-4">
+          <View
+            style={{
+              width: 50,
+              height: 50,
+            }}
+            className="flex items-center justify-center"
+          >
+            <Feather name="settings" size={28} color={"white"} />
+          </View>
+          <View className="shrink ml-2">
+            <Text className="font-bold mb-1">Centralised Settings Plugin</Text>
+            <Text className="shrink text-xs">
+              Configure settings from a centralised location on your Jellyfin
+              server. All client settings for all users will be synced
+              automatically.{" "}
+              <Text
+                className="text-purple-600"
+                onPress={() => {
+                  Linking.openURL(
+                    "https://github.com/streamyfin/jellyfin-plugin-streamyfin"
+                  );
+                }}
+              >
+                Read more
+              </Text>
+            </Text>
+          </View>
+        </View>
       </View>
-
-      <Button
-        onPress={() => {
-          router.back();
-        }}
-        className="mt-4"
-      >
-        Done
-      </Button>
-      <TouchableOpacity
-        onPress={() => {
-          router.back();
-          router.push("/settings");
-        }}
-        className="mt-4"
-      >
-        <Text className="text-purple-600 text-center">Go to settings</Text>
-      </TouchableOpacity>
+      <View>
+        <Button
+          onPress={() => {
+            router.back();
+          }}
+          className="mt-4"
+        >
+          Done
+        </Button>
+        <TouchableOpacity
+          onPress={() => {
+            router.back();
+            router.push("/settings");
+          }}
+          className="mt-4"
+        >
+          <Text className="text-purple-600 text-center">Go to settings</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
