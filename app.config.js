4f96af4d4ed4a7d45e6b41146dfe3861f48a6bd9
sarendsen <coding-mosses0z@icloud.com>
2025-01-31 13:22:51 +0100

    WIP

---
+++ app.config.js
@@ -0,0 +1,11 @@
+module.exports = ({ config }) => {
+  if (process.env.EXPO_TV === "1") {
+    config.plugins.push([
+      "react-native-google-cast",
+      { useDefaultExpandedMediaControls: true },
+    ]);
+  }
+  return {
+    ...config,
+  };
+};
