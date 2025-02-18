1ac6b7e3df01a89399e8f305b2e25878fab505c0
Fredrik Burmester <fredrik.burmester@gmail.com>
2025-02-18 17:56:10 +0100

    fix: chromecast not working

--- utils/jellyfin/media/getStreamUrl.ts
+++ utils/jellyfin/media/getStreamUrl.ts
@@ -36,6 +36,7 @@ export const getStreamUrl = async ({
   mediaSource: MediaSourceInfo | undefined;
 } | null> => {
   if (!api || !userId || !item?.Id) {
+    console.warn("Missing required parameters for getStreamUrl");
     return null;
   }
 
@@ -120,9 +121,7 @@ export const getStreamUrl = async ({
         sessionId: sessionId,
         mediaSource,
       };
-    }
-
-    if (mediaSource?.SupportsDirectPlay) {
+    } else {
       const searchParams = new URLSearchParams({
         playSessionId: sessionData?.PlaySessionId || "",
         mediaSourceId: mediaSource?.Id || "",
@@ -149,39 +148,4 @@ export const getStreamUrl = async ({
       };
     }
   }
-
-  if (item.MediaType === "Audio") {
-    if (mediaSource?.TranscodingUrl) {
-      return {
-        url: `${api.basePath}${mediaSource.TranscodingUrl}`,
-        sessionId,
-        mediaSource,
-      };
-    }
-
-    const searchParams = new URLSearchParams({
-      UserId: userId,
-      DeviceId: api.deviceInfo.id,
-      MaxStreamingBitrate: "140000000",
-      Container:
-        "opus,webm|opus,mp3,aac,m4a|aac,m4b|aac,flac,webma,webm|webma,wav,ogg",
-      TranscodingContainer: "mp4",
-      TranscodingProtocol: "hls",
-      AudioCodec: "aac",
-      api_key: api.accessToken,
-      PlaySessionId: sessionData?.PlaySessionId || "",
-      StartTimeTicks: "0",
-      EnableRedirection: "true",
-      EnableRemoteMedia: "false",
-    });
-    return {
-      url: `${
-        api.basePath
-      }/Audio/${itemId}/universal?${searchParams.toString()}`,
-      sessionId,
-      mediaSource,
-    };
-  }
-
-  throw new Error("Unsupported media type");
 };
