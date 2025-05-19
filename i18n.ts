6be586e2fb459f6517c115a5acf48bea7daf8753
SimplyJanDE <78982850+SimplyJanDE@users.noreply.github.com>
2025-05-19 12:11:56 +0200

    fix: import klingon and esperanto to translation file and fix ukranian import

--- i18n.ts
+++ i18n.ts
@@ -5,6 +5,7 @@ import { getLocales } from "expo-localization";
 import de from "./translations/de.json";
 import en from "./translations/en.json";
 import es from "./translations/es.json";
+import eo from "./translations/eo.json";
 import fr from "./translations/fr.json";
 import it from "./translations/it.json";
 import ja from "./translations/ja.json";
@@ -14,7 +15,8 @@ import ptBR from "./translations/pt-BR.json";
 import sv from "./translations/sv.json";
 import ru from "./translations/ru.json";
 import tr from "./translations/tr.json";
-import ua from "./translations/ua.json";
+import tlh from "./translations/tlh.json";
+import uk from "./translations/uk.json";
 import zhCN from "./translations/zh-CN.json";
 import zhTW from "./translations/zh-TW.json";
 
@@ -22,16 +24,18 @@ export const APP_LANGUAGES = [
   { label: "Deutsch", value: "de" },
   { label: "English", value: "en" },
   { label: "Español", value: "es" },
+  { label: "Esperanto", value: "eo" },
   { label: "Français", value: "fr" },
   { label: "Italiano", value: "it" },
   { label: "日本語", value: "ja" },
+  { label: "Klingon", value: "tlh" },
   { label: "Türkçe", value: "tr" },
   { label: "Nederlands", value: "nl" },
   { label: "Polski", value: "pl" },
   { label: "Português (Brasil)", value: "pt-BR" },
   { label: "Svenska", value: "sv" },
   { label: "Русский", value: "ru" },
-  { label: "Українська", value: "ua" },
+  { label: "Українська", value: "uk" },
   { label: "简体中文", value: "zh-CN" },
   { label: "繁體中文", value: "zh-TW" },
 ];
@@ -42,6 +46,7 @@ i18n.use(initReactI18next).init({
     de: { translation: de },
     en: { translation: en },
     es: { translation: es },
+    eo: { translation: eo },
     fr: { translation: fr },
     it: { translation: it },
     ja: { translation: ja },
@@ -51,7 +56,8 @@ i18n.use(initReactI18next).init({
     sv: { translation: sv },
     ru: { translation: ru },
     tr: { translation: tr },
-    ua: { translation: ua },
+    tlh: { translation: tlh },
+    uk: { translation: uk },
     "zh-CN": { translation: zhCN },
     "zh-TW": { translation: zhTW },
   },
