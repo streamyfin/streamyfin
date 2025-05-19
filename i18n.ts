185524c06cf2bd8c481213895bbae93c0f980e93
Chris <182387676+whoopsi-daisy@users.noreply.github.com>
2025-05-19 17:39:02 +0700

    feat(lang): add Klingon and Esperanto localization support (#672)

--- i18n.ts
+++ i18n.ts
@@ -5,6 +5,7 @@ import { getLocales } from "expo-localization";
 import de from "./translations/de.json";
 import en from "./translations/en.json";
 import es from "./translations/es.json";
+import eo from "./translations/eo.json";
 import fr from "./translations/fr.json";
 import it from "./translations/it.json";
 import ja from "./translations/ja.json";
@@ -14,6 +15,7 @@ import ptBR from "./translations/pt-BR.json";
 import ru from "./translations/ru.json";
 import sv from "./translations/sv.json";
 import tr from "./translations/tr.json";
+import tlh from "./translations/tlh.json";
 import uk from "./translations/uk.json";
 import zhCN from "./translations/zh-CN.json";
 import zhTW from "./translations/zh-TW.json";
@@ -22,15 +24,18 @@ export const APP_LANGUAGES = [
   { label: "Deutsch", value: "de" },
   { label: "English", value: "en" },
   { label: "Español", value: "es" },
+  { label: "Esperanto", value: "eo" },
   { label: "Français", value: "fr" },
   { label: "Italiano", value: "it" },
   { label: "日本語", value: "ja" },
+  { label: "Klingon", value: "tlh" },
   { label: "Türkçe", value: "tr" },
   { label: "Nederlands", value: "nl" },
   { label: "Polski", value: "pl" },
   { label: "Português (Brasil)", value: "pt-BR" },
   { label: "Svenska", value: "sv" },
   { label: "Русский", value: "ru" },
+  { label: "Українська", value: "uk" },
   { label: "Українська", value: "uk" },
   { label: "简体中文", value: "zh-CN" },
   { label: "繁體中文", value: "zh-TW" },
@@ -42,6 +47,7 @@ i18n.use(initReactI18next).init({
     de: { translation: de },
     en: { translation: en },
     es: { translation: es },
+    eo: { translation: eo },
     fr: { translation: fr },
     it: { translation: it },
     ja: { translation: ja },
@@ -51,6 +57,7 @@ i18n.use(initReactI18next).init({
     sv: { translation: sv },
     ru: { translation: ru },
     tr: { translation: tr },
+    tlh: { translation: tlh },
     uk: { translation: uk },
     "zh-CN": { translation: zhCN },
     "zh-TW": { translation: zhTW },
