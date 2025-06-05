3ffaf2b728113b7b650671f6f1d2cfa2c097f34d
WreckingBANG <86848811+WreckingBANG@users.noreply.github.com>
2025-06-05 13:56:49 +0200

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,14 +1,2 @@
 module.exports = ({ config }) => {
-  if (process.env.EXPO_TV !== "1") {
-    config.plugins.push([
-      "react-native-google-cast",
-      { useDefaultExpandedMediaControls: true },
-    ]);
-  }
-  return {
-    android: {
-      googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
-    },
-    ...config,
-  };
 };
