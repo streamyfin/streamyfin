7efbba956019ecb3bf52a355d245673a6c4233e0
WreckingBANG <86848811+WreckingBANG@users.noreply.github.com>
2025-06-05 13:58:12 +0200

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -1,2 +1,17 @@
 module.exports = ({ config }) => {
+  // Remove Google Cast plugin registration:
+  // if (process.env.EXPO_TV !== "1") {
+  //   config.plugins.push([
+  //     "react-native-google-cast",
+  //     { useDefaultExpandedMediaControls: true },
+  //   ]);
+  // }
+
+  // Keep this ONLY if you use other Google services (like Firebase):
+  return {
+    android: {
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+    },
+    ...config,
+  };
 };
