9cc62639fdc20a98674c5af65e3103d478ca92b0
Endrit Beqiri <75754293+endritbeqiri@users.noreply.github.com>
2025-07-15 08:38:00 +0200

    feat: Added Albanian and Danish translations (#657)
    Co-authored-by: retardgerman <78982850+retardgerman@users.noreply.github.com>
    Co-authored-by: Gauvain <68083474+Gauvino@users.noreply.github.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- i18n.ts
+++ i18n.ts
@@ -1,6 +1,7 @@
 import { getLocales } from "expo-localization";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import da from "./translations/da.json";
 import de from "./translations/de.json";
 import en from "./translations/en.json";
 import eo from "./translations/eo.json";
@@ -13,6 +14,7 @@ import pl from "./translations/pl.json";
 import ptBR from "./translations/pt-BR.json";
 import ro from "./translations/ro.json";
 import ru from "./translations/ru.json";
+import sq from "./translations/sq.json";
 import sv from "./translations/sv.json";
 import tlh from "./translations/tlh.json";
 import tr from "./translations/tr.json";
@@ -21,6 +23,7 @@ import zhCN from "./translations/zh-CN.json";
 import zhTW from "./translations/zh-TW.json";
 
 export const APP_LANGUAGES = [
+  { label: "Dansk", value: "da" },
   { label: "Deutsch", value: "de" },
   { label: "English", value: "en" },
   { label: "Español", value: "es" },
@@ -35,6 +38,7 @@ export const APP_LANGUAGES = [
   { label: "Português (Brasil)", value: "pt-BR" },
   { label: "Română", value: "ro" },
   { label: "Svenska", value: "sv" },
+  { label: "Shqip", value: "sq" },
   { label: "Русский", value: "ru" },
   { label: "Українська", value: "uk" },
   { label: "Українська", value: "uk" },
@@ -45,6 +49,7 @@ export const APP_LANGUAGES = [
 i18n.use(initReactI18next).init({
   compatibilityJSON: "v4",
   resources: {
+    da: { translation: da },
     de: { translation: de },
     en: { translation: en },
     es: { translation: es },
@@ -57,6 +62,7 @@ i18n.use(initReactI18next).init({
     "pt-BR": { translation: ptBR },
     ro: { translation: ro },
     sv: { translation: sv },
+    sq: { translation: sq },
     ru: { translation: ru },
     tr: { translation: tr },
     tlh: { translation: tlh },
