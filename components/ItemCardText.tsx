b81bba46102dff72489b47d3fac8eb1b3c462675
Simrit Nijjar <simritnijjar@outlook.com>
2025-10-18 13:27:51 -0700

    Updated season item text to use {series} // {season}

--- components/ItemCardText.tsx
+++ components/ItemCardText.tsx
@@ -1,5 +1,6 @@
 import type { BaseItemDto } from "@jellyfin/sdk/lib/generated-client/models";
 import type React from "react";
+import { useMemo } from "react";
 import { View } from "react-native";
 import { Text } from "./common/Text";
 
@@ -8,27 +9,50 @@ type ItemCardProps = {
 };
 
 export const ItemCardText: React.FC<ItemCardProps> = ({ item }) => {
-  return (
-    <View className='mt-2 flex flex-col'>
-      {item.Type === "Episode" ? (
-        <>
-          <Text numberOfLines={1} ellipsizeMode='tail' className=''>
-            {item.Name}
-          </Text>
-          <Text numberOfLines={1} className='text-xs opacity-50'>
-            {`S${item.ParentIndexNumber?.toString()}:E${item.IndexNumber?.toString()}`}
-            {" - "}
-            {item.SeriesName}
-          </Text>
-        </>
-      ) : (
-        <>
-          <Text numberOfLines={1} ellipsizeMode='tail'>
-            {item.Name}
-          </Text>
-          <Text className='text-xs opacity-50'>{item.ProductionYear}</Text>
-        </>
-      )}
-    </View>
-  );
+  const createCardText = (
+    title: string | number | null | undefined,
+    subtitle: string | number | null | undefined,
+  ) => {
+    return (
+      <>
+        <Text numberOfLines={1} ellipsizeMode='tail'>
+          {title}
+        </Text>
+        <Text numberOfLines={1} className='text-xs opacity-50'>
+          {subtitle}
+        </Text>
+      </>
+    );
+  };
+
+  const episodeText = useMemo(() => {
+    const season = item.ParentIndexNumber?.toString() || "";
+    const episode = item.IndexNumber?.toString() || "";
+
+    return createCardText(
+      item.Name,
+      `S${season}:E${episode} - ${item.SeriesName}`,
+    );
+  }, [item]);
+
+  const seasonText = useMemo(() => {
+    return createCardText(item.SeriesName, item.Name);
+  }, [item]);
+
+  const otherText = useMemo(() => {
+    return createCardText(item.Name, item.ProductionYear);
+  }, [item]);
+
+  const text = useMemo(() => {
+    switch (item.Type) {
+      case "Episode":
+        return episodeText;
+      case "Season":
+        return seasonText;
+      default:
+        return otherText;
+    }
+  }, [episodeText, otherText]);
+
+  return <View className='mt-2 flex flex-col'>{text}</View>;
 };
