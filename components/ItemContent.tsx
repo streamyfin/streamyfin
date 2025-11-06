30165529959bd66c1bbb6d246f863a978109a918
lostb1t <coding-mosses0z@icloud.com>
2025-11-06 13:34:55 +0100

    Update ItemContent.tsx

--- components/ItemContent.tsx
+++ components/ItemContent.tsx
@@ -49,7 +49,6 @@ export type SelectedOptions = {
 interface ItemContentProps {
   item: BaseItemDto;
   isOffline: boolean;
-  mediaSourcesItem: BaseItemDto;
 }
 
 export const ItemContent: React.FC<ItemContentProps> = React.memo(
