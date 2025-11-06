847c185fefa23020a53461c8c656ab319a0e8253
lostb1t <coding-mosses0z@icloud.com>
2025-11-06 12:59:56 +0100

    Update useItemQuery.ts

--- hooks/useItemQuery.ts
+++ hooks/useItemQuery.ts
@@ -1,27 +1,39 @@
 import { getUserLibraryApi } from "@jellyfin/sdk/lib/utils/api";
+import { ItemFields } from "@jellyfin/sdk/lib/generated-client/models";
 import { useQuery } from "@tanstack/react-query";
 import { useAtom } from "jotai";
 import { useDownload } from "@/providers/DownloadProvider";
 import { apiAtom, userAtom } from "@/providers/JellyfinProvider";
 
-export const useItemQuery = (itemId: string, isOffline: boolean) => {
+export const useItem = (
+  itemId: string | undefined,
+  fields?: ItemFields[],
+  isOffline?: boolean
+) => {
   const [api] = useAtom(apiAtom);
   const [user] = useAtom(userAtom);
   const { getDownloadedItemById } = useDownload();
 
   return useQuery({
-    queryKey: ["item", itemId],
+    queryKey: ["item", itemId, fields],
     queryFn: async () => {
+      if (!itemId) throw new Error('Item ID is required');
+      
       if (isOffline) {
         return getDownloadedItemById(itemId)?.item;
       }
-      if (!api || !user || !itemId) return null;
-      const res = await getUserLibraryApi(api).getItem({
-        itemId: itemId,
-        userId: user?.Id,
+      
+      if (!api || !user) return null;
+      
+      const response = await getUserLibraryApi(api).getItem({
+        itemId,
+        userId: user.Id,
+        ...(fields && { fields }),
       });
-      return res.data;
+      
+      return response.data;
     },
+    enabled: !!itemId,
     staleTime: 0,
     refetchOnMount: true,
     refetchOnWindowFocus: true,
