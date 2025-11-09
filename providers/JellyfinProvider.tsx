d9b223324becc9b79d5f6590aa10bb41d9b0f5a0
Fredrik Burmester <fredrik.burmester@gmail.com>
2025-11-09 11:07:51 +0100

    chore: version

--- providers/JellyfinProvider.tsx
+++ providers/JellyfinProvider.tsx
@@ -64,7 +64,7 @@ export const JellyfinProvider: React.FC<{ children: ReactNode }> = ({
       setJellyfin(
         () =>
           new Jellyfin({
-            clientInfo: { name: "Streamyfin", version: "0.40.4" },
+            clientInfo: { name: "Streamyfin", version: "0.44.0" },
             deviceInfo: {
               name: deviceName,
               id,
@@ -87,7 +87,7 @@ export const JellyfinProvider: React.FC<{ children: ReactNode }> = ({
     return {
       authorization: `MediaBrowser Client="Streamyfin", Device=${
         Platform.OS === "android" ? "Android" : "iOS"
-      }, DeviceId="${deviceId}", Version="0.40.4"`,
+      }, DeviceId="${deviceId}", Version="0.44.0"`,
     };
   }, [deviceId]);
 
