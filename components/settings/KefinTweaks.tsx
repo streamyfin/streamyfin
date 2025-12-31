85923ff189fc63cd41ae493fc9d292fbc12a6071
Simon-Eklundh <simon.eklundh@proton.me>
2025-12-31 22:44:05 +0100

    working. might need visual work

--- components/settings/KefinTweaks.tsx
+++ components/settings/KefinTweaks.tsx
@@ -1,31 +1,32 @@
 import { useTranslation } from "react-i18next";
-import { View } from "react-native";
+import { Switch, Text, View } from "react-native";
 import { useSettings } from "@/utils/atoms/settings";
-import { Button } from "../Button";
-import { Text } from "../common/Text";
 
 export const KefinTweaksSettings = () => {
   const { settings, updateSettings } = useSettings();
   const { t } = useTranslation();
+
+  const isEnabled = settings?.useKefinTweaks ?? false;
+
   return (
     <View className=''>
-      <View>
-        {
-          <View className='flex flex-col rounded-xl overflow-hidden p-4 bg-neutral-900'>
-            <Text className='text-xs text-red-600 mb-2'>
-              {t("home.settings.plugins.kefinTweaks.watchlist_enabler")}
-            </Text>
-            <Button
-              color='purple'
-              className='h-12 mt-2'
-              onPress={() => {
-                updateSettings({ useKefinTweaks: !settings?.useKefinTweaks });
-              }}
-            >
-              {t("home.settings.plugins.kefinTweaks.watchlist_button")}
-            </Button>
-          </View>
-        }
+      <View className='flex flex-col rounded-xl overflow-hidden p-4 bg-neutral-900'>
+        <Text className='text-xs text-red-600 mb-2'>
+          {t("home.settings.plugins.kefinTweaks.watchlist_enabler")}
+        </Text>
+
+        <View className='flex flex-row items-center justify-between mt-2'>
+          <Text className='text-white'>
+            {isEnabled ? t("Watchlist On") : t("Watchlist Off")}
+          </Text>
+
+          <Switch
+            value={isEnabled}
+            onValueChange={(value) => updateSettings({ useKefinTweaks: value })}
+            trackColor={{ false: "#555", true: "purple" }}
+            thumbColor={isEnabled ? "#fff" : "#ccc"}
+          />
+        </View>
       </View>
     </View>
   );
