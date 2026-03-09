25f45b3404031778ca20d643a2589af3ebedba5a
chrysanm <mk.chrysanthou@gmail.com>
2026-03-09 22:15:34 +0100

    fix(player): re-enable controls auto-hide timeout

--- components/video-player/controls/Controls.tsx
+++ components/video-player/controls/Controls.tsx
@@ -445,7 +445,7 @@ export const Controls: FC<Props> = ({
     episodeView,
     onHideControls: hideControls,
     timeout: CONTROLS_CONSTANTS.TIMEOUT,
-    disabled: true,
+    disabled: false,
   });
 
   const switchOnEpisodeMode = useCallback(() => {
