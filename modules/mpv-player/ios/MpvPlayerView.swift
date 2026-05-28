c949f4952c78d8de4d3c58bfbc3fec5065194043
Jeff Stein <jeffstein@gmail.com>
2026-05-28 13:54:37 -0600

    fix: remove duplicate mpv now playing stub

--- modules/mpv-player/ios/MpvPlayerView.swift
+++ modules/mpv-player/ios/MpvPlayerView.swift
@@ -429,16 +429,6 @@ class MpvPlayerView: ExpoView {
 		return renderer?.getTechnicalInfo() ?? [:]
 	}
 
-	// MARK: - Now Playing Metadata
-
-	private var nowPlayingMetadata: [String: String] = [:]
-
-	func setNowPlayingMetadata(_ metadata: [String: String]) {
-		nowPlayingMetadata = metadata
-		// TODO: Implement MPNowPlayingInfoCenter integration for Control Center
-		// This would set the title, artist, artwork, etc. for the system Now Playing UI
-	}
-
 	deinit {
 		if let observer = appStateObserver {
 			NotificationCenter.default.removeObserver(observer)
