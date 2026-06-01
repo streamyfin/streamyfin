21fb05658626c93b85e9672f3ebb15c8795951df
Gauvain <contact@uruk.dev>
2026-06-01 09:46:27 +0200

    fix(i18n): make two hardcoded titles translatable (#1627)

--- modules/mpv-player/src/MpvPlayerView.web.tsx
+++ modules/mpv-player/src/MpvPlayerView.web.tsx
@@ -1,11 +1,13 @@
+import { useTranslation } from "react-i18next";
 import { MpvPlayerViewProps } from "./MpvPlayer.types";
 
 export default function MpvPlayerView(props: MpvPlayerViewProps) {
   const url = props.source?.url ?? "";
+  const { t } = useTranslation();
   return (
     <div>
       <iframe
-        title='MPV Player'
+        title={t("player.mpv_player_title")}
         style={{ flex: 1 }}
         src={url}
         onLoad={() => props.onLoad?.({ nativeEvent: { url } })}
